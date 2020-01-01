88fedef241e38fdc71d8116ef53d0947fba8138b
Frank Kiwy <frank.kiwy@outlook.com>
2020-01-01 20:23:14 +0100

    Fixed a bug in the Arrow class

--- src/main/java/astro/tool/box/module/Arrow.java
+++ src/main/java/astro/tool/box/module/Arrow.java
@@ -5,41 +5,43 @@ import static java.lang.Math.*;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Arrow implements Drawable {
+public class Arrow {
 
-    private final int x1;
-    private final int y1;
-    private final int x2;
-    private final int y2;
-    private final int size;
+    private final double x1;
+    private final double y1;
+    private final double x2;
+    private final double y2;
+    private final double size;
     private final Color color;
 
     public Arrow(double x1, double y1, double x2, double y2, double size, Color color) {
-        this.x1 = (int) round(x1);
-        this.y1 = (int) round(y1);
-        this.x2 = (int) round(x2);
-        this.y2 = (int) round(y2);
-        this.size = (int) round(size);
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.size = size;
         this.color = color;
     }
 
-    @Override
     public void draw(Graphics graphics) {
-        double theta = Math.atan2(y2 - y1, x2 - x1);
-        double phi = Math.toRadians(20);
-        double rho;
+        double xDiff = x1 - x2;
+        double vectorAngle;
+        if (xDiff == 0) {
+            vectorAngle = PI / 2;
+        } else {
+            vectorAngle = atan((y1 - y2) / xDiff) + (x1 < x2 ? PI : 0);
+        }
+        double arrowAngle = PI / 7;
+        double xLeft = size * cos(vectorAngle - arrowAngle);
+        double yLeft = size * sin(vectorAngle - arrowAngle);
+        double xRight = size * cos(vectorAngle + arrowAngle);
+        double yRight = size * sin(vectorAngle + arrowAngle);
+        //double xPlus = (size / 2) * cos(vectorAngle);
+        //double yPlus = (size / 2) * sin(vectorAngle);
         graphics.setColor(color);
-        rho = theta + phi;
-        drawSide(graphics, rho);
-        rho = theta - phi;
-        drawSide(graphics, rho);
-        graphics.drawLine(x1, y1, x2, y2);
-    }
-
-    private void drawSide(Graphics graphics, double rho) {
-        int x = (int) round(x2 - size * cos(rho));
-        int y = (int) round(y2 - size * sin(rho));
-        graphics.drawLine(x2, y2, x, y);
+        graphics.drawLine((int) round(x2), (int) round(y2), (int) round(x2 + xLeft), (int) round(y2 + yLeft));
+        graphics.drawLine((int) round(x2), (int) round(y2), (int) round(x2 + xRight), (int) round(y2 + yRight));
+        graphics.drawLine((int) round(x1), (int) round(y1), (int) round(x2 /*+ xPlus*/), (int) round(y2 /*+ yPlus*/));
     }
 
 }
