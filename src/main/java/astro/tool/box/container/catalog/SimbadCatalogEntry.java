a60c7d4c246ca7808106f6eb8be8639b046742a2
Frank Kiwy <frank.kiwy@outlook.com>
2020-01-04 11:07:52 +0100

    Fixed a bug in the SimbadCatalogEntry class: plx was shown instead of plx_err

--- src/main/java/astro/tool/box/container/catalog/SimbadCatalogEntry.java
+++ src/main/java/astro/tool/box/container/catalog/SimbadCatalogEntry.java
@@ -158,7 +158,7 @@ public class SimbadCatalogEntry implements CatalogEntry {
         catalogElements.add(new CatalogElement("ra", roundTo7DecNZ(ra), Alignment.LEFT, getDoubleComparator()));
         catalogElements.add(new CatalogElement("dec", roundTo7DecNZ(dec), Alignment.LEFT, getDoubleComparator()));
         catalogElements.add(new CatalogElement("plx (mas)", roundTo4DecNZ(plx), Alignment.RIGHT, getDoubleComparator(), true));
-        catalogElements.add(new CatalogElement("plx err", roundTo4DecNZ(plx), Alignment.RIGHT, getDoubleComparator()));
+        catalogElements.add(new CatalogElement("plx err", roundTo4DecNZ(plx_err), Alignment.RIGHT, getDoubleComparator()));
         catalogElements.add(new CatalogElement("pmra (mas/yr)", roundTo3DecNZ(pmra), Alignment.RIGHT, getDoubleComparator(), true));
         catalogElements.add(new CatalogElement("pmdec (mas/yr)", roundTo3DecNZ(pmdec), Alignment.RIGHT, getDoubleComparator(), true));
         catalogElements.add(new CatalogElement("rad vel (km/s)", roundTo1DecNZ(radvel), Alignment.RIGHT, getDoubleComparator(), true));
