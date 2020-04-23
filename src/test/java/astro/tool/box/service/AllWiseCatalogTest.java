9752af66785b4d2deebbfab2d10f37e99b7713fc
Frank Kiwy <frank.kiwy@outlook.com>
2020-04-23 09:43:12 +0200

    Refactored unit tests

--- src/test/java/astro/tool/box/service/AllWiseCatalogTest.java
+++ src/test/java/astro/tool/box/service/AllWiseCatalogTest.java
@@ -5,11 +5,6 @@ import static astro.tool.box.util.Constants.*;
 import static astro.tool.box.util.ServiceProviderUtils.*;
 import static astro.tool.box.util.TestData.*;
 
-import astro.tool.box.container.catalog.AllWiseCatalogEntry;
-import astro.tool.box.facade.CatalogQueryFacade;
-import astro.tool.box.proxy.CatalogQueryProxy;
-import astro.tool.box.container.catalog.CatalogEntry;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
@@ -19,35 +14,8 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.Before;
 
 public class AllWiseCatalogTest {
-
-    CatalogQueryFacade catalogQueryProxy = new CatalogQueryProxy();
-    CatalogQueryFacade catalogQueryService = new CatalogQueryService();
-    AllWiseCatalogEntry catalogEntry = new AllWiseCatalogEntry();
-
-    @Before
-    public void init() {
-        catalogEntry.setRa(DEG_RA);
-        catalogEntry.setDec(DEG_DE);
-        catalogEntry.setSearchRadius(DEG_RADIUS);
-    }
-
-    @Test
-    public void printProxyCatalogEntries() throws IOException {
-        List<CatalogEntry> entriesFromProxy = catalogQueryProxy.getCatalogEntriesByCoords(catalogEntry);
-        entriesFromProxy.forEach((entry) -> {
-            System.out.println(entry);
-        });
-    }
-
-    @Test
-    public void getCatalogEntriesByCoords() throws IOException {
-        List<CatalogEntry> entriesFromProxy = catalogQueryProxy.getCatalogEntriesByCoords(catalogEntry);
-        List<CatalogEntry> entriesFromService = catalogQueryService.getCatalogEntriesByCoords(catalogEntry);
-        assertEquals(entriesFromProxy.get(0), entriesFromService.get(0));
-    }
 
     @Test
     public void parseResponse() throws IOException {
@@ -59,13 +27,13 @@ public class AllWiseCatalogTest {
 
         BufferedReader reader = new BufferedReader(new StringReader(readResponse(connection)));
         List<String[]> results = reader.lines().map(line -> {
-            //System.out.println(line);
+            System.out.println(line);
             return line.split(SPLIT_CHAR);
         }).collect(Collectors.toList());
 
         String[] header = results.get(0);
         for (int i = 0; i < header.length; i++) {
-            System.out.println(header[i] + " : " + i);
+            System.out.println(i + ": " + header[i]);
         }
     }
 
