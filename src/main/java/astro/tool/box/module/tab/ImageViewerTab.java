487619656e92d1ba4f3902dec3ca78b0e0d0bad0
Frank Kiwy <frank.kiwy@outlook.com>
2020-12-01 18:57:53 +0100

    Added a border around subtracted contrast label

--- src/main/java/astro/tool/box/module/tab/ImageViewerTab.java
+++ src/main/java/astro/tool/box/module/tab/ImageViewerTab.java
@@ -232,6 +232,7 @@ public class ImageViewerTab {
     private JLabel lowScaleLabel;
     private JLabel subScaleLabel;
     private JLabel epochLabel;
+    private JPanel subScalePanel;
     private JPanel zooniversePanel1;
     private JPanel zooniversePanel2;
     private JCheckBox unwiseCutouts;
@@ -541,12 +542,14 @@ public class ImageViewerTab {
                     autoContrast.setSelected(true);
                     optimizeContrast.setSelected(false);
                     blurImages.setSelected(true);
-                    subScaleSlider.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+                    subScalePanel.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 1, Color.RED.darker()));
+                    subScaleSlider.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.RED.darker()));
                     setContrast(LOW_CONTRAST, HIGH_CONTRAST);
                     setSubContrast(subContrastSaved);
                 } else if (Epoch.isSubtracted(previousEpoch)) {
                     optimizeContrast.setSelected(true);
                     blurImages.setSelected(false);
+                    subScalePanel.setBorder(BorderFactory.createEmptyBorder());
                     subScaleSlider.setBorder(BorderFactory.createEmptyBorder());
                     setContrast(lowContrastSaved, highContrastSaved);
                     setSubContrast(SUB_CONTRAST);
@@ -609,12 +612,12 @@ public class ImageViewerTab {
                 }
             });
 
-            whitePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            controlPanel.add(whitePanel);
-            whitePanel.setBackground(Color.WHITE);
+            subScalePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            controlPanel.add(subScalePanel);
+            subScalePanel.setBackground(Color.WHITE);
 
             subScaleLabel = new JLabel(String.format(SUB_SCALE_LABEL, subContrast));
-            whitePanel.add(subScaleLabel);
+            subScalePanel.add(subScaleLabel);
 
             subScaleSlider = new JSlider(1, 19, SUB_CONTRAST);
             controlPanel.add(subScaleSlider);
