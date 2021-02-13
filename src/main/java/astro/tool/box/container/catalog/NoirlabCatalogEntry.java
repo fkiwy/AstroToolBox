ff70a013e7bd513afd958bab1394733ed4b507a0
Frank Kiwy <frank.kiwy@outlook.com>
2021-02-13 13:50:51 +0100

    Added further constraints to the NSC DR2 proper motion query

--- src/main/java/astro/tool/box/container/catalog/NoirlabCatalogEntry.java
+++ src/main/java/astro/tool/box/container/catalog/NoirlabCatalogEntry.java
@@ -361,7 +361,9 @@ public class NoirlabCatalogEntry implements CatalogEntry, ProperMotionQuery {
     private String createProperMotionQuery() {
         StringBuilder query = new StringBuilder();
         addRow(query, createCatalogQuery());
+        addRow(query, "AND   ndet >= 5");
         addRow(query, "AND   deltamjd >= 180");
+        addRow(query, "AND   class_star >= 0.7");
         addRow(query, "AND   pmra <> 'NaN' AND pmdec <> 'NaN'");
         addRow(query, "AND   SQRT(pmra * pmra + pmdec * pmdec) >= " + tpm);
         return query.toString();
