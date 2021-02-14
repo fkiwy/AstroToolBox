4362faec51f01b42c319830031c6d502e8452eef
Frank Kiwy <frank.kiwy@outlook.com>
2021-02-14 09:41:07 +0100

    Changed program version number to 2.3.3 Removed any unwanted characters from the user input coordinates

--- src/main/java/astro/tool/box/module/ModuleHelper.java
+++ src/main/java/astro/tool/box/module/ModuleHelper.java
@@ -92,7 +92,7 @@ import org.json.JSONObject;
 public class ModuleHelper {
 
     public static final String PGM_NAME = "AstroToolBox";
-    public static final String PGM_VERSION = "2.3.2";
+    public static final String PGM_VERSION = "2.3.3";
     public static final String CONFIG_FILE_URL = "https://drive.google.com/uc?export=download&id=1RYT_nJA7oO6HgoFkLpq0CWqspXCgcp3I";
     public static final String DOWNLOAD_URL = "https://drive.google.com/file/d/";
 
@@ -241,11 +241,11 @@ public class ModuleHelper {
         Pattern pattern = Pattern.compile(".*[a-zA-Z]+.*");
         Matcher matcher = pattern.matcher(coords);
         if (matcher.matches()) {
-            NameResolverService nameResolverService = new NameResolverService();
             try {
+                NameResolverService nameResolverService = new NameResolverService();
                 coords = nameResolverService.getCoordinatesByName(coords);
             } catch (Exception ex) {
-                showErrorDialog(null, ex.getMessage());
+                coords = coords.replaceAll("[^\\d .-]", "");
             }
         }
         String[] parts = splitCoordinates(coords);
