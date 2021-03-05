7601a79392074a319eb6f8e7369c800e81c1e61c
Frank Kiwy <frank.kiwy@outlook.com>
2021-03-05 17:02:11 +0100

    Added 2 radio buttons to switch between IRSA and VizieR TAP for AllWISE, CatWISE, Gaia DR2 and 2MASS catalogs

--- src/main/java/astro/tool/box/module/tab/SettingsTab.java
+++ src/main/java/astro/tool/box/module/tab/SettingsTab.java
@@ -52,7 +52,7 @@ public class SettingsTab {
     public static final String PROP_FILE_NAME = "/AstroToolBox.properties";
     public static final String PROP_PATH = USER_HOME + PROP_FILE_NAME;
     public static final Properties USER_SETTINGS = new Properties();
-    public static String DEFAULT_TAP_PROVIDER = TapProvider.IRSA.name();
+    public static String DEFAULT_TAP_PROVIDER = TapProvider.VIZIER.name();
     public static String DEFAULT_LOOK_AND_FEEL = LookAndFeel.OS.name();
     public static String CURRENT_LOOK_AND_FEEL;
 
