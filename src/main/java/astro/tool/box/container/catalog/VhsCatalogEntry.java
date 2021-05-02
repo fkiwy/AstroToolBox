e59630999637964b55842a98d399129af8bb75ea
Frank Kiwy <frank.kiwy@outlook.com>
2021-05-02 17:58:24 +0200

    Added the Time Series tab

--- src/main/java/astro/tool/box/container/catalog/VhsCatalogEntry.java
+++ src/main/java/astro/tool/box/container/catalog/VhsCatalogEntry.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-//
+
 public class VhsCatalogEntry implements CatalogEntry {
 
     public static final String CATALOG_NAME = "VHS DR5";
