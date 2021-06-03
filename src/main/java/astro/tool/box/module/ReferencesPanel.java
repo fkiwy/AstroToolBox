288333a911a60a72676171118101918dafc85dd6
Frank Kiwy <frank.kiwy@outlook.com>
2021-06-03 17:10:41 +0200

    Added the "References" button to the SIMBAD details panel of the Image Viewer

--- src/main/java/astro/tool/box/module/ReferencesPanel.java
+++ src/main/java/astro/tool/box/module/ReferencesPanel.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -54,231 +55,139 @@ public class ReferencesPanel extends JPanel {
                 String mainIdentifier = catalogEntry.getSourceId();
 
                 // Object types
-                List<String[]> objectTypes = Collections.EMPTY_LIST; //simbadQueryService.getObjectTypes(mainIdentifier);
+                List<String[]> results = Collections.EMPTY_LIST; //simbadQueryService.getObjectTypes(mainIdentifier);
 
                 String[] columns = new String[]{"Object type", "Description"};
-                Object[][] rows = new Object[][]{};
-
-                JTable objectTypesTable = new JTable(objectTypes.toArray(rows), columns);
-                alignResultColumns(objectTypesTable, objectTypes);
-                resizeColumnWidth(objectTypesTable, 1000);
-                objectTypesTable.setAutoCreateRowSorter(true);
-                objectTypesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                JScrollPane resultPanel = new JScrollPane(objectTypesTable);
+                JScrollPane resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Object types"));
 
                 measurementsPanel.add(resultPanel);
 
                 // Spectral types
-                List<String[]> spectralTypes = simbadQueryService.getObjectSpectralTypes(mainIdentifier);
+                results = simbadQueryService.getObjectSpectralTypes(mainIdentifier);
 
-                columns = new String[]{"Spectral type", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable spectalTypesTable = new JTable(spectralTypes.toArray(rows), columns);
-                alignResultColumns(spectalTypesTable, spectralTypes);
-                resizeColumnWidth(spectalTypesTable, 1000);
-                spectalTypesTable.setAutoCreateRowSorter(true);
-                spectalTypesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(spectalTypesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Spectral types"));
 
                 measurementsPanel.add(resultPanel);
 
                 // Parallaxes
-                List<String[]> parallaxes = simbadQueryService.getObjectParallaxes(mainIdentifier);
+                results = simbadQueryService.getObjectParallaxes(mainIdentifier);
 
                 columns = new String[]{"Parallax", "Error", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable parallaxesTable = new JTable(parallaxes.toArray(rows), columns);
-                alignResultColumns(parallaxesTable, parallaxes);
-                resizeColumnWidth(parallaxesTable, 1000);
-                parallaxesTable.setAutoCreateRowSorter(true);
-                parallaxesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(parallaxesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Parallaxes"));
 
                 measurementsPanel.add(resultPanel);
 
                 // Distances
-                List<String[]> distances = simbadQueryService.getObjectDistances(mainIdentifier);
+                results = simbadQueryService.getObjectDistances(mainIdentifier);
 
                 columns = new String[]{"Distance", "Quality", "Unit", "Minus error", "Plus error", "Method", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable distancesTable = new JTable(distances.toArray(rows), columns);
-                alignResultColumns(distancesTable, distances);
-                resizeColumnWidth(distancesTable, 1000);
-                distancesTable.setAutoCreateRowSorter(true);
-                distancesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(distancesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Distances"));
 
                 measurementsPanel.add(resultPanel);
 
                 // Velocities
-                List<String[]> velocities = simbadQueryService.getObjectVelocities(mainIdentifier);
+                results = simbadQueryService.getObjectVelocities(mainIdentifier);
 
                 columns = new String[]{"Type", "Velocity", "Error", "Quality", "Number of meas.", "Nature of meas.", "Quality", "Wavelength", "Resolution", "Obs. date", "Remarks", "Origin", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable velocitiesTable = new JTable(velocities.toArray(rows), columns);
-                alignResultColumns(velocitiesTable, velocities);
-                resizeColumnWidth(velocitiesTable, 1000);
-                velocitiesTable.setAutoCreateRowSorter(true);
-                velocitiesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(velocitiesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Velocities"));
 
                 measurementsPanel.add(resultPanel);
 
                 // Proper motions
-                List<String[]> properMotions = simbadQueryService.getObjectProperMotions(mainIdentifier);
+                results = simbadQueryService.getObjectProperMotions(mainIdentifier);
 
                 columns = new String[]{"PM RA", "PM DE", "RA error", "DE error", "Coord. system", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable properMotionsTable = new JTable(properMotions.toArray(rows), columns);
-                alignResultColumns(properMotionsTable, properMotions);
-                resizeColumnWidth(properMotionsTable, 1000);
-                properMotionsTable.setAutoCreateRowSorter(true);
-                properMotionsTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(properMotionsTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Proper motions"));
 
                 measurementsPanel2.add(resultPanel);
 
                 // Fluxes
-                List<String[]> fluxes = simbadQueryService.getObjectFluxes(mainIdentifier);
+                results = simbadQueryService.getObjectFluxes(mainIdentifier);
 
                 columns = new String[]{"Filter", "Flux", "Error", "Quality", "Description", "Unit", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable fluxesTable = new JTable(fluxes.toArray(rows), columns);
-                alignResultColumns(fluxesTable, fluxes);
-                resizeColumnWidth(fluxesTable, 1000);
-                fluxesTable.setAutoCreateRowSorter(true);
-                fluxesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(fluxesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Fluxes"));
 
                 measurementsPanel2.add(resultPanel);
 
                 // Variabilities
-                List<String[]> variabilities = simbadQueryService.getObjectVariabilities(mainIdentifier);
+                results = simbadQueryService.getObjectVariabilities(mainIdentifier);
 
                 columns = new String[]{"Type", "Upper limit flag", "Max. brightness", "Uncertainty flag", "Magnitude type", "Lower limit flag", "Min. brightness", "Uncertainty flag", "Lower limit flag period", "Period", "Uncertainty flag period", "Epoch", "Uncertainty epoch", "Raising time", "Uncertainty raising time", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable variabilitiesTable = new JTable(variabilities.toArray(rows), columns);
-                alignResultColumns(variabilitiesTable, variabilities);
-                resizeColumnWidth(variabilitiesTable, 1000);
-                variabilitiesTable.setAutoCreateRowSorter(true);
-                variabilitiesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(variabilitiesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Variabilities"));
 
                 measurementsPanel2.add(resultPanel);
 
                 // Rotations
-                List<String[]> rotations = simbadQueryService.getObjectRotations(mainIdentifier);
+                results = simbadQueryService.getObjectRotations(mainIdentifier);
 
                 columns = new String[]{"Upper value Vsini", "Vsini", "Error", "Number of meas.", "Quality", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable rotationsTable = new JTable(rotations.toArray(rows), columns);
-                alignResultColumns(rotationsTable, rotations);
-                resizeColumnWidth(rotationsTable, 1000);
-                rotationsTable.setAutoCreateRowSorter(true);
-                rotationsTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(rotationsTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Rotations"));
 
                 measurementsPanel2.add(resultPanel);
 
                 // Metallicities
-                List<String[]> metallicities = simbadQueryService.getObjectMetallicities(mainIdentifier);
+                results = simbadQueryService.getObjectMetallicities(mainIdentifier);
 
                 columns = new String[]{"Teff", "Log g", "Fe H", "Fe H flag", "Comparison star", "Star in the Cayrel et al.", "Bibcode"};
-                rows = new Object[][]{};
-
-                JTable metallicitiesTable = new JTable(metallicities.toArray(rows), columns);
-                alignResultColumns(metallicitiesTable, metallicities);
-                resizeColumnWidth(metallicitiesTable, 1000);
-                metallicitiesTable.setAutoCreateRowSorter(true);
-                metallicitiesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-                resultPanel = new JScrollPane(metallicitiesTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 150));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Metallicities"));
 
                 measurementsPanel2.add(resultPanel);
 
                 // Object identifiers
-                List<String[]> identifiers = simbadQueryService.getObjectIdentifiers(mainIdentifier);
+                results = simbadQueryService.getObjectIdentifiers(mainIdentifier);
 
                 columns = new String[]{"Identifier"};
-                rows = new Object[][]{};
-
-                JTable identifiersTable = new JTable(identifiers.toArray(rows), columns);
-                alignResultColumns(identifiersTable, identifiers);
-                resizeColumnWidth(identifiersTable, 1000);
-                identifiersTable.setAutoCreateRowSorter(true);
-                identifiersTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-                identifiersTable.setTableHeader(null);
-
-                resultPanel = new JScrollPane(identifiersTable);
+                resultPanel = new JScrollPane(createResultTable(results, columns));
                 resultPanel.setPreferredSize(new Dimension(300, 200));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Object identifiers"));
 
                 referencesPanel.add(resultPanel);
 
                 // Object references
-                List<String[]> references = simbadQueryService.getObjectReferences(mainIdentifier);
+                results = simbadQueryService.getObjectReferences(mainIdentifier);
 
                 columns = new String[]{"Year", "Journal", "Volume", "Title", "Bibcode", "Ref"};
-                rows = new Object[][]{};
+                JComponent component = createResultTable(results, columns);
+                if (component instanceof JTable) {
+                    JTable resultTable = (JTable) component;
+                    resultTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                    resultTable.getSelectionModel().addListSelectionListener((ListSelectionEvent e) -> {
+                        if (!e.getValueIsAdjusting()) {
+                            String bibRef = (String) resultTable.getValueAt(resultTable.getSelectedRow(), 5);
+                            populateDetailsPanel(Integer.valueOf(bibRef), detailsPanel);
 
-                JTable referencesTable = new JTable(references.toArray(rows), columns);
-                alignResultColumns(referencesTable, references);
-                resizeColumnWidth(referencesTable, 1000);
-                referencesTable.setAutoCreateRowSorter(true);
-                referencesTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-                referencesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                referencesTable.getSelectionModel().addListSelectionListener((ListSelectionEvent e) -> {
-                    if (!e.getValueIsAdjusting()) {
-                        String bibRef = (String) referencesTable.getValueAt(referencesTable.getSelectedRow(), 5);
-                        populateDetailsPanel(Integer.valueOf(bibRef), detailsPanel);
+                            String bibCode = (String) resultTable.getValueAt(resultTable.getSelectedRow(), 4);
+                            populateLinksPanel(bibCode, linksPanel);
 
-                        String bibCode = (String) referencesTable.getValueAt(referencesTable.getSelectedRow(), 4);
-                        populateLinksPanel(bibCode, linksPanel);
+                            populateCatalogsPanel(bibCode, catalogsPanel);
 
-                        populateCatalogsPanel(bibCode, catalogsPanel);
+                            referencesFrame.setVisible(true);
+                        }
+                    });
+                    component = resultTable;
+                }
 
-                        referencesFrame.setVisible(true);
-                    }
-                });
-
-                resultPanel = new JScrollPane(referencesTable);
+                resultPanel = new JScrollPane(component);
                 resultPanel.setPreferredSize(new Dimension(1500, 200));
                 resultPanel.setBorder(ModuleHelper.createEtchedBorder("Object references"));
 
@@ -298,19 +207,10 @@ public class ReferencesPanel extends JPanel {
             detailsPanel.removeAll();
 
             // Authors
-            List<String[]> authors = simbadQueryService.getAuthors(bibRef);
+            List<String[]> results = simbadQueryService.getAuthors(bibRef);
 
             String[] columns = new String[]{"Author"};
-            Object[][] rows = new Object[][]{};
-
-            JTable authorsTable = new JTable(authors.toArray(rows), columns);
-            alignResultColumns(authorsTable, authors);
-            resizeColumnWidth(authorsTable, 1000);
-            authorsTable.setAutoCreateRowSorter(true);
-            authorsTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-            authorsTable.setTableHeader(null);
-
-            JScrollPane resultPanel = new JScrollPane(authorsTable);
+            JScrollPane resultPanel = new JScrollPane(createResultTable(results, columns));
             resultPanel.setPreferredSize(new Dimension(300, 200));
             resultPanel.setBorder(ModuleHelper.createEtchedBorder("Authors"));
 
@@ -358,6 +258,19 @@ public class ReferencesPanel extends JPanel {
         } catch (IOException ex) {
             showExceptionDialog(null, ex);
         }
+    }
+
+    private JComponent createResultTable(List<String[]> results, String[] columns) {
+        if (results.isEmpty()) {
+            return new JLabel("N/A");
+        }
+        Object[][] rows = new Object[][]{};
+        JTable resultTable = new JTable(results.toArray(rows), columns);
+        alignResultColumns(resultTable, results);
+        resizeColumnWidth(resultTable, 1000);
+        resultTable.setAutoCreateRowSorter(true);
+        resultTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        return resultTable;
     }
 
 }
