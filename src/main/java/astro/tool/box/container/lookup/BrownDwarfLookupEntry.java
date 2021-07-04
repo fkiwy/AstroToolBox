8a4664deb331a6dc9a3e2059d5d578870ccb8ec1
Frank Kiwy <frank.kiwy@outlook.com>
2021-07-04 15:47:57 +0200

    Added colors for M0-M5 spectral types to the Brown dwarf lookup table

--- src/main/java/astro/tool/box/container/lookup/BrownDwarfLookupEntry.java
+++ src/main/java/astro/tool/box/container/lookup/BrownDwarfLookupEntry.java
@@ -45,7 +45,7 @@ public class BrownDwarfLookupEntry implements MainSequenceLookup {
         colors.put(Color.W1_W2, toDouble(values[20]));
         colors.put(Color.J_K, toDouble(values[25]));
         colors.put(Color.i_z_DES, toDouble(values[27]));
-        colors.put(Color.z_Y_DES, toDouble(values[28]));
+        //colors.put(Color.z_Y_DES, toDouble(values[28]));
         colors.put(Color.i_z, toDouble(values[30]));
         colors.put(Color.z_Y, toDouble(values[31]));
         colors.put(Color.G_RP, toDouble(values[33]));
