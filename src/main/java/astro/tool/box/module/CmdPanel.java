290774b8060d1502bd9c754169de423bbc33f2a9
Frank Kiwy <frank.kiwy@outlook.com>
2021-10-17 11:41:21 +0200

    Added the Montreal cooling sequences for white dwarfs to the Gaia CMD

--- src/main/java/astro/tool/box/module/CmdPanel.java
+++ src/main/java/astro/tool/box/module/CmdPanel.java
@@ -6,6 +6,7 @@ import static astro.tool.box.module.ModuleHelper.*;
 import static astro.tool.box.module.tab.SettingsTab.*;
 import astro.tool.box.container.NumberTriplet;
 import astro.tool.box.container.catalog.GaiaCmd;
+import astro.tool.box.util.CSVParser;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -15,11 +16,16 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -38,28 +44,31 @@ public class CmdPanel extends JPanel {
 
     private static final String FONT_NAME = "Tahoma";
 
+    private final JRadioButton g_rpButton;
+    private final JCheckBox coolingSequencesH;
+
+    private final int min = 2;
+    private final int max = 14;
+
+    private JFreeChart chart;
+
     public CmdPanel(GaiaCmd catalogEntry) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         JPanel commandPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        JRadioButton g_rpButton = new JRadioButton("G-RP", true);
+        g_rpButton = new JRadioButton("G-RP", true);
         commandPanel.add(g_rpButton);
-        g_rpButton.addActionListener((ActionEvent e) -> {
-            remove(0);
-            createChartPanel(catalogEntry, true);
-        });
 
         JRadioButton bp_rpButton = new JRadioButton("BP-RP", false);
         commandPanel.add(bp_rpButton);
-        bp_rpButton.addActionListener((ActionEvent e) -> {
-            remove(0);
-            createChartPanel(catalogEntry, false);
-        });
 
         ButtonGroup groupOne = new ButtonGroup();
         groupOne.add(g_rpButton);
         groupOne.add(bp_rpButton);
+
+        coolingSequencesH = new JCheckBox("Pure-H");
+        commandPanel.add(coolingSequencesH);
 
         String info = "Right-clicking on the chart, opens a context menu with additional functions like printing and saving.";
 
@@ -73,15 +82,39 @@ public class CmdPanel extends JPanel {
 
         loadCmdData();
 
-        createChartPanel(catalogEntry, true);
+        chart = createChartPanel(catalogEntry);
+
+        g_rpButton.addActionListener((ActionEvent e) -> {
+            remove(0);
+            chart = createChartPanel(catalogEntry);
+            if (coolingSequencesH.isSelected()) {
+                addCoolingSequences(chart);
+            }
+        });
+
+        bp_rpButton.addActionListener((ActionEvent e) -> {
+            remove(0);
+            chart = createChartPanel(catalogEntry);
+            if (coolingSequencesH.isSelected()) {
+                addCoolingSequences(chart);
+            }
+        });
+
+        coolingSequencesH.addActionListener((ActionEvent e) -> {
+            if (coolingSequencesH.isSelected()) {
+                addCoolingSequences(chart);
+            } else {
+                removeCoolingSequences(chart);
+            }
+        });
 
         add(commandPanel);
     }
 
-    private void createChartPanel(GaiaCmd catalogEntry, boolean g_rp) {
-        XYSeriesCollection collection = createCollection(g_rp);
-        XYSeriesCollection collectionTarget = createCollectionTarget(catalogEntry, g_rp);
-        JFreeChart chart = createChart(collection, collectionTarget, g_rp);
+    private JFreeChart createChartPanel(GaiaCmd catalogEntry) {
+        XYSeriesCollection mainCollection = createMainCollection();
+        XYSeriesCollection targetCollection = createTargetCollection(catalogEntry);
+        JFreeChart chart = createChart(targetCollection, mainCollection);
         ChartPanel chartPanel = new ChartPanel(chart) {
             @Override
             public void mouseDragged(MouseEvent e) {
@@ -92,13 +125,14 @@ public class CmdPanel extends JPanel {
         add(chartPanel, 0);
         revalidate();
         repaint();
+        return chart;
     }
 
-    private XYSeriesCollection createCollection(boolean g_rp) {
+    private XYSeriesCollection createMainCollection() {
         XYSeriesCollection collection = new XYSeriesCollection();
         XYSeries series = new XYSeries("");
         CMD_DATA.forEach(triplet -> {
-            double x = g_rp ? triplet.getY() : triplet.getZ();
+            double x = g_rpButton.isSelected() ? triplet.getY() : triplet.getZ();
             double y = triplet.getX();
             if (x != 0 && y != 0) {
                 series.add(x, y);
@@ -108,28 +142,42 @@ public class CmdPanel extends JPanel {
         return collection;
     }
 
-    private XYSeriesCollection createCollectionTarget(GaiaCmd catalogEntry, boolean g_rp) {
+    private XYSeriesCollection createSequenceCollection(String sequenceName) {
         XYSeriesCollection collection = new XYSeriesCollection();
-        double xTarget = g_rp ? catalogEntry.getG_RP() : catalogEntry.getBP_RP();
+        XYSeries series = new XYSeries(sequenceName, false);
+        loadCoolingSequence(sequenceName).forEach(triplet -> {
+            double x = g_rpButton.isSelected() ? triplet.getY() : triplet.getZ();
+            double y = triplet.getX();
+            if (x != 0 && y != 0) {
+                series.add(x, y);
+            }
+        });
+        collection.addSeries(series);
+        return collection;
+    }
+
+    private XYSeriesCollection createTargetCollection(GaiaCmd catalogEntry) {
+        XYSeriesCollection collection = new XYSeriesCollection();
+        double xTarget = g_rpButton.isSelected() ? catalogEntry.getG_RP() : catalogEntry.getBP_RP();
         double yTarget = catalogEntry.getAbsoluteGmag();
         if (xTarget != 0 && yTarget != 0) {
             XYSeries seriesTarget = new XYSeries(catalogEntry.getCatalogName() + " " + catalogEntry.getSourceId()
-                    + ": G=" + roundTo3DecNZ(yTarget) + " " + (g_rp ? "G-RP" : "BP-RP") + "=" + roundTo3DecNZ(xTarget));
+                    + ": G=" + roundTo3DecNZ(yTarget) + " " + (g_rpButton.isSelected() ? "G-RP" : "BP-RP") + "=" + roundTo3DecNZ(xTarget));
             seriesTarget.add(xTarget, yTarget);
             collection.addSeries(seriesTarget);
         }
         return collection;
     }
 
-    private JFreeChart createChart(XYSeriesCollection collection, XYSeriesCollection collectionTarget, boolean g_rp) {
+    private JFreeChart createChart(XYSeriesCollection targetCollection, XYSeriesCollection mainCollection) {
         JFreeChart chart = ChartFactory.createXYLineChart("Gaia Color-Magnitude Diagram", "", "", null);
         XYPlot plot = chart.getXYPlot();
-        plot.setDataset(0, collectionTarget);
-        plot.setDataset(1, collection);
+        plot.setDataset(0, targetCollection);
+        plot.setDataset(1, mainCollection);
 
-        NumberAxis xAxis = new NumberAxis(g_rp ? "G-RP" : "BP-RP");
+        NumberAxis xAxis = new NumberAxis(g_rpButton.isSelected() ? "G-RP" : "BP-RP");
         xAxis.setTickUnit(new NumberTickUnit(0.5));
-        xAxis.setRange(-0.5, g_rp ? 2.5 : 5.5);
+        xAxis.setRange(g_rpButton.isSelected() ? -0.5 : -1.0, g_rpButton.isSelected() ? 2.5 : 5.5);
         plot.setDomainAxis(xAxis);
 
         NumberAxis yAxis = new NumberAxis("Abs G (mag)");
@@ -144,27 +192,25 @@ public class CmdPanel extends JPanel {
         xAxis.setLabelFont(labelFont);
         yAxis.setLabelFont(labelFont);
 
-        XYLineAndShapeRenderer rendererTarget = new XYLineAndShapeRenderer();
-        rendererTarget.setSeriesPaint(0, Color.RED);
-        rendererTarget.setSeriesLinesVisible(0, false);
-
         double size = 10.0;
         double delta = size / 2.0;
-        //Shape shape = new Rectangle2D.Double(-delta, -delta, size, size);
         Shape shape = new Ellipse2D.Double(-delta, -delta, size, size);
-        rendererTarget.setSeriesShape(0, shape);
-        plot.setRenderer(0, rendererTarget);
-
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesPaint(0, Color.BLACK);
-        renderer.setSeriesLinesVisible(0, false);
-        renderer.setSeriesVisibleInLegend(0, false);
+        XYLineAndShapeRenderer targetRenderer = new XYLineAndShapeRenderer();
+        targetRenderer.setSeriesPaint(0, Color.RED);
+        targetRenderer.setSeriesLinesVisible(0, false);
+        targetRenderer.setSeriesVisibleInLegend(0, true);
+        targetRenderer.setSeriesShape(0, shape);
+        plot.setRenderer(0, targetRenderer);
 
         size = 0.2;
         delta = size / 2.0;
         shape = new Ellipse2D.Double(-delta, -delta, size, size);
-        renderer.setSeriesShape(0, shape);
-        plot.setRenderer(1, renderer);
+        XYLineAndShapeRenderer mainRenderer = new XYLineAndShapeRenderer();
+        mainRenderer.setSeriesPaint(0, Color.BLACK);
+        mainRenderer.setSeriesLinesVisible(0, false);
+        mainRenderer.setSeriesVisibleInLegend(0, false);
+        mainRenderer.setSeriesShape(0, shape);
+        plot.setRenderer(1, mainRenderer);
 
         plot.setBackgroundPaint(Color.WHITE);
         plot.setRangeGridlinesVisible(true);
@@ -181,6 +227,46 @@ public class CmdPanel extends JPanel {
         chart.getTitle().setFont(titleFont);
 
         return chart;
+    }
+
+    private void addCoolingSequences(JFreeChart chart) {
+        XYPlot plot = chart.getXYPlot();
+        for (int i = min; i < max; i++) {
+            String mass = roundTo1Dec((double) i / 10);
+            plot.setDataset(i, createSequenceCollection(String.format("Mass %s H", mass)));
+            plot.setDataset(i + max - min, createSequenceCollection(String.format("Mass %s He", mass)));
+        }
+
+        double size = 0.2;
+        double delta = size / 2.0;
+        Shape shape = new Ellipse2D.Double(-delta, -delta, size, size);
+
+        XYLineAndShapeRenderer sequenceRendererH = new XYLineAndShapeRenderer();
+        sequenceRendererH.setSeriesPaint(0, Color.RED);
+        sequenceRendererH.setSeriesLinesVisible(0, true);
+        sequenceRendererH.setSeriesVisibleInLegend(0, false);
+        sequenceRendererH.setSeriesShape(0, shape);
+
+        XYLineAndShapeRenderer sequenceRendererHe = new XYLineAndShapeRenderer();
+        sequenceRendererHe.setSeriesPaint(0, Color.BLUE);
+        sequenceRendererHe.setSeriesLinesVisible(0, true);
+        sequenceRendererHe.setSeriesVisibleInLegend(0, false);
+        sequenceRendererHe.setSeriesShape(0, shape);
+
+        for (int i = min; i < max; i++) {
+            plot.setRenderer(i, sequenceRendererH);
+            plot.setRenderer(i + max - min, sequenceRendererHe);
+        }
+    }
+
+    private void removeCoolingSequences(JFreeChart chart) {
+        XYSeriesCollection targetCollection;
+        for (int i = min; i < max; i++) {
+            targetCollection = (XYSeriesCollection) chart.getXYPlot().getDataset(i);
+            targetCollection.removeAllSeries();
+            targetCollection = (XYSeriesCollection) chart.getXYPlot().getDataset(i + max - min);
+            targetCollection.removeAllSeries();
+        }
     }
 
     private void loadCmdData() {
@@ -205,6 +291,29 @@ public class CmdPanel extends JPanel {
         } catch (FileNotFoundException ex) {
             throw new RuntimeException(ex.getMessage());
         }
+    }
+
+    private List<NumberTriplet> loadCoolingSequence(String sequenceName) {
+        List<NumberTriplet> coolingSequence = new ArrayList();
+        InputStream input = getClass().getResourceAsStream("/sequences/" + sequenceName + ".csv");
+        try (Scanner fileScanner = new Scanner(input)) {
+            String headerLine = fileScanner.nextLine();
+            String[] headers = CSVParser.parseLine(headerLine);
+            Map<String, Integer> columns = new HashMap<>();
+            for (int i = 0; i < headers.length; i++) {
+                columns.put(headers[i], i);
+            }
+            while (fileScanner.hasNextLine()) {
+                String bodyLine = fileScanner.nextLine();
+                String[] values = CSVParser.parseLine(bodyLine);
+                double G = toDouble(values[columns.get("G3")]);
+                double BP = toDouble(values[columns.get("G3_BP")]);
+                double RP = toDouble(values[columns.get("G3_RP")]);
+                //System.out.println(G + " " + BP + " " + RP);
+                coolingSequence.add(new NumberTriplet(G, G - RP, BP - RP));
+            }
+        }
+        return coolingSequence;
     }
 
 }
