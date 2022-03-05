9c217e306f3be14d06e79306072d7177d3b5c6db
Frank Kiwy <54667906+fkiwy@users.noreply.github.com>
2022-03-05 20:06:37 +0100

    Added a setting to enable/disable photometric uncertainties in spectral type estimates

--- src/main/java/astro/tool/box/lookup/SpectralTypeLookupEntry.java
+++ src/main/java/astro/tool/box/lookup/SpectralTypeLookupEntry.java
@@ -1,6 +1,7 @@
 package astro.tool.box.lookup;
 
 import static astro.tool.box.function.NumericFunctions.*;
+import static astro.tool.box.tab.SettingsTab.*;
 import astro.tool.box.enumeration.Color;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,43 +29,58 @@ public class SpectralTypeLookupEntry implements MainSequenceLookup {
         rsun = toDouble(values[29]);
         msun = toDouble(values[30]);
         colors = new HashMap<>();
+
         // Gaia
         colors.put(Color.BP_RP, toDouble(values[10]));
         colors.put(Color.G_RP, toDouble(values[11]));
         colors.put(Color.M_G, toDouble(values[12]));
-        colors.put(Color.e_BP_RP, toDouble(values[10]));
-        colors.put(Color.e_G_RP, toDouble(values[11]));
-        colors.put(Color.e_M_G, toDouble(values[12]));
-        colors.put(Color.E_BP_RP, toDouble(values[10]));
-        colors.put(Color.E_G_RP, toDouble(values[11]));
-        colors.put(Color.E_M_G, toDouble(values[12]));
+
         // 2MASS
-        //colors.put(Color.J_H, toDouble(values[18]));
-        //colors.put(Color.H_K, toDouble(values[19]));
-        //colors.put(Color.e_J_H, toDouble(values[18]));
-        //colors.put(Color.e_H_K, toDouble(values[19]));
-        //colors.put(Color.E_J_H, toDouble(values[18]));
-        //colors.put(Color.E_H_K, toDouble(values[19]));
+        colors.put(Color.J_H, toDouble(values[18]));
+        colors.put(Color.H_K, toDouble(values[19]));
+
         // WISE
         colors.put(Color.K_W1, toDouble(values[20]));
         colors.put(Color.W1_W2, toDouble(values[21]));
         colors.put(Color.W1_W3, toDouble(values[22]));
         colors.put(Color.W1_W4, toDouble(values[23]));
-        colors.put(Color.e_K_W1, toDouble(values[20]));
-        colors.put(Color.e_W1_W2, toDouble(values[21]));
-        colors.put(Color.e_W1_W3, toDouble(values[22]));
-        colors.put(Color.e_W1_W4, toDouble(values[23]));
-        colors.put(Color.E_K_W1, toDouble(values[20]));
-        colors.put(Color.E_W1_W2, toDouble(values[21]));
-        colors.put(Color.E_W1_W3, toDouble(values[22]));
-        colors.put(Color.E_W1_W4, toDouble(values[23]));
+
         // SDSS
-        //colors.put(Color.g_r, toDouble(values[26]));
+        colors.put(Color.g_r, toDouble(values[26]));
         colors.put(Color.i_z, toDouble(values[27]));
-        //colors.put(Color.e_g_r, toDouble(values[26]));
-        colors.put(Color.e_i_z, toDouble(values[27]));
-        //colors.put(Color.E_g_r, toDouble(values[26]));
-        colors.put(Color.E_i_z, toDouble(values[27]));
+
+        boolean photometricErrors = Boolean.parseBoolean(getUserSetting(PHOTOMETRIC_ERRORS, "false"));
+        if (photometricErrors) {
+            // Gaia
+            colors.put(Color.e_BP_RP, toDouble(values[10]));
+            colors.put(Color.e_G_RP, toDouble(values[11]));
+            colors.put(Color.e_M_G, toDouble(values[12]));
+            colors.put(Color.E_BP_RP, toDouble(values[10]));
+            colors.put(Color.E_G_RP, toDouble(values[11]));
+            colors.put(Color.E_M_G, toDouble(values[12]));
+
+            // 2MASS
+            colors.put(Color.e_J_H, toDouble(values[18]));
+            colors.put(Color.e_H_K, toDouble(values[19]));
+            colors.put(Color.E_J_H, toDouble(values[18]));
+            colors.put(Color.E_H_K, toDouble(values[19]));
+
+            // WISE
+            colors.put(Color.e_K_W1, toDouble(values[20]));
+            colors.put(Color.e_W1_W2, toDouble(values[21]));
+            colors.put(Color.e_W1_W3, toDouble(values[22]));
+            colors.put(Color.e_W1_W4, toDouble(values[23]));
+            colors.put(Color.E_K_W1, toDouble(values[20]));
+            colors.put(Color.E_W1_W2, toDouble(values[21]));
+            colors.put(Color.E_W1_W3, toDouble(values[22]));
+            colors.put(Color.E_W1_W4, toDouble(values[23]));
+
+            // SDSS
+            colors.put(Color.e_g_r, toDouble(values[26]));
+            colors.put(Color.e_i_z, toDouble(values[27]));
+            colors.put(Color.E_g_r, toDouble(values[26]));
+            colors.put(Color.E_i_z, toDouble(values[27]));
+        }
     }
 
     @Override
--- src/main/java/astro/tool/box/lookup/WhiteDwarfLookupEntry.java
+++ src/main/java/astro/tool/box/lookup/WhiteDwarfLookupEntry.java
@@ -1,6 +1,7 @@
 package astro.tool.box.lookup;
 
 import static astro.tool.box.function.NumericFunctions.*;
+import static astro.tool.box.tab.SettingsTab.*;
 import astro.tool.box.enumeration.Color;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,33 +29,45 @@ public class WhiteDwarfLookupEntry implements SpectralTypeLookup {
         msun = toDouble(values[32]);
         age = values[33];
         colors = new HashMap<>();
+
         // 2MASS
         colors.put(Color.J_H, toDouble(values[21]));
         colors.put(Color.H_K, toDouble(values[22]));
-        colors.put(Color.e_J_H, toDouble(values[21]));
-        colors.put(Color.e_H_K, toDouble(values[22]));
-        colors.put(Color.E_J_H, toDouble(values[21]));
-        colors.put(Color.E_H_K, toDouble(values[22]));
+
         // SDSS
-        //colors.put(Color.u_g, toDouble(values[23]));
+        colors.put(Color.u_g, toDouble(values[23]));
         colors.put(Color.g_r, toDouble(values[24]));
         colors.put(Color.r_i, toDouble(values[25]));
         colors.put(Color.i_z, toDouble(values[26]));
-        //colors.put(Color.e_u_g, toDouble(values[23]));
-        colors.put(Color.e_g_r, toDouble(values[24]));
-        colors.put(Color.e_r_i, toDouble(values[25]));
-        colors.put(Color.e_i_z, toDouble(values[26]));
-        //colors.put(Color.E_u_g, toDouble(values[23]));
-        colors.put(Color.E_g_r, toDouble(values[24]));
-        colors.put(Color.E_r_i, toDouble(values[25]));
-        colors.put(Color.E_i_z, toDouble(values[26]));
+
         // Gaia
         colors.put(Color.G_RP, toDouble(values[29]));
         colors.put(Color.BP_RP, toDouble(values[30]));
-        colors.put(Color.e_G_RP, toDouble(values[29]));
-        colors.put(Color.e_BP_RP, toDouble(values[30]));
-        colors.put(Color.E_G_RP, toDouble(values[29]));
-        colors.put(Color.E_BP_RP, toDouble(values[30]));
+
+        boolean photometricErrors = Boolean.parseBoolean(getUserSetting(PHOTOMETRIC_ERRORS, "false"));
+        if (photometricErrors) {
+            // 2MASS
+            colors.put(Color.e_J_H, toDouble(values[21]));
+            colors.put(Color.e_H_K, toDouble(values[22]));
+            colors.put(Color.E_J_H, toDouble(values[21]));
+            colors.put(Color.E_H_K, toDouble(values[22]));
+
+            // SDSS
+            colors.put(Color.e_u_g, toDouble(values[23]));
+            colors.put(Color.e_g_r, toDouble(values[24]));
+            colors.put(Color.e_r_i, toDouble(values[25]));
+            colors.put(Color.e_i_z, toDouble(values[26]));
+            colors.put(Color.E_u_g, toDouble(values[23]));
+            colors.put(Color.E_g_r, toDouble(values[24]));
+            colors.put(Color.E_r_i, toDouble(values[25]));
+            colors.put(Color.E_i_z, toDouble(values[26]));
+
+            // Gaia
+            colors.put(Color.e_G_RP, toDouble(values[29]));
+            colors.put(Color.e_BP_RP, toDouble(values[30]));
+            colors.put(Color.E_G_RP, toDouble(values[29]));
+            colors.put(Color.E_BP_RP, toDouble(values[30]));
+        }
     }
 
     @Override
