a7c4efcce75f9455fdfe1f7d365918bacb69a24a
Frank Kiwy <frank.kiwy@outlook.com>
2023-11-01 20:17:26 +0100

    Remove K type templates from SED select box

--- src/main/java/astro/tool/box/enumeration/SpectralType.java
+++ src/main/java/astro/tool/box/enumeration/SpectralType.java
@@ -2,6 +2,6 @@ package astro.tool.box.enumeration;
 
 public enum SpectralType {
 
-    SELECT, K0, K1, K2, K3, K4, K5, K6, K7, K8, K9, M0, M1, M2, M3, M4, M5, M6, M7, M8, M9, L0, L1, L2, L3, L4, L5, L6, L7, L8, L9, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9
+    SELECT, M0, M1, M2, M3, M4, M5, M6, M7, M8, M9, L0, L1, L2, L3, L4, L5, L6, L7, L8, L9, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9
 
 }
