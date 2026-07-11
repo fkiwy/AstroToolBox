5312a15250b5a5ad0e1e3282bd529cfd8cfeeae4
Frank Kiwy <frank.kiwy@outlook.com>
2026-07-11 10:47:39 +0200

    Add median photometric distance to SED plots

--- src/main/java/astro/tool/box/container/SedBestMatch.java
+++ src/main/java/astro/tool/box/container/SedBestMatch.java
@@ -1,5 +1,9 @@
 package astro.tool.box.container;
 
+import java.util.List;
+
+import astro.tool.box.enumeration.Band;
+
 public class SedBestMatch {
 
 	private final String spt;
@@ -8,10 +12,20 @@ public class SedBestMatch {
 
 	private final double meanDiffMag;
 
-	public SedBestMatch(String spt, double medianDiffMag, double meanDiffMag) {
+	private final double medianPhotDist;
+
+	private final double stdPhotDist;
+
+	private final List<Band> photBands;
+
+	public SedBestMatch(String spt, double medianDiffMag, double meanDiffMag, double medianPhotDist, double stdPhotDist,
+			List<Band> photBands) {
 		this.spt = spt;
 		this.medianDiffMag = medianDiffMag;
 		this.meanDiffMag = meanDiffMag;
+		this.medianPhotDist = medianPhotDist;
+		this.stdPhotDist = stdPhotDist;
+		this.photBands = photBands;
 	}
 
 	public String getSpt() {
@@ -24,6 +38,18 @@ public class SedBestMatch {
 
 	public double getMeanDiffMag() {
 		return meanDiffMag;
+	}
+
+	public double getMedianPhotDist() {
+		return medianPhotDist;
+	}
+
+	public double getStdPhotDist() {
+		return stdPhotDist;
+	}
+
+	public List<Band> getPhotBands() {
+		return photBands;
 	}
 
 }
